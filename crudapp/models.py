60ff87f034444ad8ee8954e5db80a728e38ecb25
Sharmarke Said <sharmakesaid17@gmail.com@gmail.com>
2023-11-23 08:37:33 +0300

    Added Student Modal

--- crudapp/models.py
+++ crudapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    address = models.CharField(max_length=255)
+    student_class = models.CharField(max_length=255)
