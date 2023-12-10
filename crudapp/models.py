cda53b2854a402ab499c8994cd34dcfe61b53b35
Sharmarke Said <sharmakesaid17@gmail.com@gmail.com>
2023-12-10 11:31:45 +0300

    Solved some issues about image handling, especially deleting image when a student is deleted or updated

--- crudapp/models.py
+++ crudapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 class Student(models.Model):
@@ -8,5 +8,19 @@ class Student(models.Model):
     address = models.CharField(max_length=255)
     student_class = models.CharField(max_length=255)
     student_image = models.ImageField(null=True, blank=True, upload_to='student_images/')
+
     def __str__(self) -> str:
         return f"{self.name} - {self.email} - {self.address} - {self.student_class}"
+
+
+    def save(self, *args, **kwargs):
+        # Check if the object already exists in the database
+        if self.pk is not None:
+            # Retrieve the existing object
+            existing_student = Student.objects.get(pk=self.pk)
+            # Check if the existing object has an image
+            if existing_student.student_image:
+                # Delete the existing image file
+                os.remove(existing_student.student_image.path)
+
+        super().save(*args, **kwargs)
